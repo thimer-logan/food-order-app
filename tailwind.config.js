f8b19868568699a1a98be758fe325e6d1b49958d
Logan Thimer <thimerlogan@gmail.com>
2023-10-14 10:11:07 -0600

    Imported google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        notosans: ['"Noto Sans JP"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };

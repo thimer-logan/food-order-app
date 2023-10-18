75b7dd97516cceaf3973229d741bbf67d7a674dc
Logan Thimer <thimerlogan@gmail.com>
2023-10-18 06:41:58 -0600

    Created a header component

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import mealsImage from "../../assets/images/meals.jpg";
+import HeaderButton from "../ui/HeaderButton";
+
+interface HeaderProps {
+  onShowCart: () => void;
+}
+
+function Header({ onShowCart }: HeaderProps) {
+  return (
+    <>
+      <header className="fixed top-0 left-0 w-full h-20 bg-[#8a2b06] text-white flex justify-between items-center p-0 px-[10%] shadow-md z-10">
+        <h1 className="text-4xl font-bold">ReactMeals</h1>
+        <HeaderButton onClick={onShowCart} />
+      </header>
+      <div className="w-full h-96 z-0 overflow-hidden">
+        <img
+          className="w-full h-full object-cover -rotate-[5deg] -translate-x-4 -translate-y-16"
+          src={mealsImage}
+          alt="meals"
+        />
+      </div>
+    </>
+  );
+}
+
+export default Header;

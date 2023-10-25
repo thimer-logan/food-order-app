94b8baeff541b0a021f0c8d29dbf888a67d8e5cf
Logan Thimer <thimerlogan@gmail.com>
2023-10-25 07:03:35 -0600

    Integrated redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/layout/Header";
+import { MealData } from "./types/MealData";
+import MealItemList from "./components/meals/MealItemList";
+import { Provider } from "react-redux";
+import store from "./store";
+import Cart from "./components/cart/Cart";
 
 function App() {
+  const [cartVisible, setCartVisible] = useState(false);
+
+  const showCartHandler = (): void => {
+    setCartVisible(true);
+  };
+
+  const hideCartHandler = (): void => {
+    setCartVisible(false);
+  };
+
+  const meals: MealData[] = [
+    {
+      id: 381778,
+      title: "Cowboy Burger",
+      price: 21.12,
+      imageUrl: "https://spoonacular.com/menuItemImages/hamburger.jpg",
+    },
+    {
+      id: 321356,
+      title: "99 Top a Burger, Sauteed Peppers, 2 oz",
+      price: 18.79,
+      imageUrl: "https://spoonacular.com/menuItemImages/hamburger.jpg",
+    },
+    {
+      id: 227166,
+      title: "Chicken Penne Pasta",
+      price: 24.49,
+      imageUrl:
+        "https://images.spoonacular.com/file/wximages/227166-312x231.jpg",
+    },
+    {
+      id: 353683,
+      title: "Dan Dan Noodles Original w/ Steak",
+      price: 25.49,
+      imageUrl:
+        "https://images.spoonacular.com/file/wximages/353683-312x231.jpg",
+    },
+    {
+      id: 239036,
+      title: "Penne Pasta, for Seafood Pasta Bake",
+      price: 25.79,
+      imageUrl: "https://spoonacular.com/menuItemImages/seafood-pasta.jpg",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      {cartVisible && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <MealItemList meals={meals} />
+      </main>
+    </Provider>
   );
 }
 
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import mealsImage from "../../assets/images/meals.jpg";
 import HeaderButton from "../ui/HeaderButton";
+import { useAppSelector } from "../../hooks/hooks";
 
 interface HeaderProps {
   onShowCart: () => void;
 }
 
 function Header({ onShowCart }: HeaderProps) {
+  const items = useAppSelector((state) => state.cart.items);
+
   return (
     <>
       <header className="fixed top-0 left-0 w-full h-20 bg-[#8a2b06] text-white flex justify-between items-center p-0 px-[10%] shadow-md z-10">
         <h1 className="text-4xl font-bold">ReactMeals</h1>
-        <HeaderButton onClick={onShowCart} />
+        <HeaderButton
+          onClick={onShowCart}
+          badge={items.reduce((acc, item) => {
+            return acc + item.quantity;
+          }, 0)}
+        />
       </header>
       <div className="w-full h-96 z-0 overflow-hidden">
         <img

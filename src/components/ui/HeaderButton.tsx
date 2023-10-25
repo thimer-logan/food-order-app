baf53da7e81e1e05bbfba225ba87b7bb1b959297
Logan Thimer <thimerlogan@gmail.com>
2023-10-25 07:00:20 -0600

    Added badge prop to display number of items

--- src/components/ui/HeaderButton.tsx
+++ src/components/ui/HeaderButton.tsx
@@ -3,9 +3,10 @@ import { BsCartFill } from "react-icons/bs";
 
 interface HeaderButtonProps {
   onClick: () => void;
+  badge: number;
 }
 
-function HeaderButton({ onClick }: HeaderButtonProps) {
+function HeaderButton({ onClick, badge }: HeaderButtonProps) {
   return (
     <button
       className="cursor-pointer bg-[#4d1601] text-white px-12 py-3 flex justify-around items-center rounded-3xl font-bold active:bg-[#2c0d00] hover:bg-[#2c0d00]"
@@ -14,7 +15,7 @@ function HeaderButton({ onClick }: HeaderButtonProps) {
       <BsCartFill className="w-6 h-6 mr-2" />
       <span>Your Cart</span>
       <span className="bg-[#b94517] px-4 py-1 rounded-3xl ml-4 font-bold">
-        2
+        {badge}
       </span>
     </button>
   );

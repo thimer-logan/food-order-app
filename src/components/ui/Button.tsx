28573b430d9ecf137807563edd89c46c8e9a58cd
Logan Thimer <thimerlogan@gmail.com>
2023-10-25 07:01:02 -0600

    Styling changes and added className prop

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 interface ButtonProps {
+  className?: string;
   onClick?: () => void;
   children: React.ReactNode;
 }
 
-function Button({ onClick, children }: ButtonProps) {
+function Button({ className, onClick, children }: ButtonProps) {
+  const defaultStyles =
+    "cursor-pointer bg-[#8a2b06] rounded-2xl border-solid border-2 border-[#8a2b06] font-bold active:bg-[#641e03] hover:bg-[#641e03]";
   return (
-    <button
-      className="cursor-pointer bg-[#8a2b06] text-white px-8 py-1 rounded-2xl border-solid border-2 border-[#8a2b06] font-bold active:bg-[#641e03] hover:bg-[#641e03]"
-      onClick={onClick}
-    >
+    <button className={`${defaultStyles} ${className}`} onClick={onClick}>
       {children}
     </button>
   );
